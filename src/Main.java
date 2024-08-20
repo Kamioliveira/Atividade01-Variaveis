8d3d5344a42b9c06e381846259adcb0aa6fd059d
kauedev <131567044+KaueCoelho@users.noreply.github.com>
2024-08-19 22:19:14 -0300

    Ajustes aula

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+// criacão de variaveis
+        int idade = 22;
+        double altura = 1.80;
+        String nome = "Matheus";
+        boolean isEstudante = true;
+
+// Operaçoes simples
+        int somaIdades = idade + 19;
+        double alturaDobro = altura * 2;
+        String saudacao = "Oi," + nome + "!";
+
+        // Exibição de Resultados
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nome: " + nome);
+        System.out.println("Is Estudante: " + isEstudante);
+        System.out.println("Soma Idades: " + somaIdades);
+        System.out.println("Saudacao: " + saudacao);
+    }
+}
